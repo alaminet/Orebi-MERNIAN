9a465bc89d3ec2c3992335ec75fef5370cbb62ee
Al Amin ET <alaminet.bd@gmail.com>
2024-05-27 00:29:29 +0600

    view-product-video-option-added

--- dashboard/components/ViewProduct.jsx
+++ dashboard/components/ViewProduct.jsx
@@ -46,7 +46,34 @@ const ViewProduct = () => {
         }
       );
       let prdListArr = [];
+
       productList?.data.map((item, i) => {
+        // Use a regular expression to find the oembed element in the HTML string
+        const oembedRegex = /<oembed[^>]*>/g;
+        const oembedMatch = item?.discription?.match(oembedRegex);
+        // If an oembed element was found, convert it to an iframe element
+        if (oembedMatch) {
+          const oembedUrl = oembedMatch[0].match(/url="([^"]*)"/)[1];
+          if (oembedUrl.match(".be/")) {
+            const iframeElement = `<iframe width="315" height="200" src="https://www.youtube.com/embed/${
+              oembedUrl.split(".be/")[1]
+            }" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
+            item.discription = item?.discription?.replace(
+              oembedRegex,
+              iframeElement
+            );
+          }
+          if (oembedUrl.match("watch")) {
+            const iframeElement = `<iframe width="315" height="200" src="https://www.youtube.com/embed/${
+              oembedUrl.split("?v=")[1].split("&")[0]
+            }" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
+            item.discription = item?.discription?.replace(
+              oembedRegex,
+              iframeElement
+            );
+          }
+          // console.log(oembedUrl.split("?v=")[1].split("&amp;")[0]);
+        }
         prdListArr.push({
           dataIndex: i,
           title: item.title,
